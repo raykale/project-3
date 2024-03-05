d681ff2433a601e94778049fe0d6d4dcdfc641c7
rachail Pomerlee <rachailpomerlee@rachails-MBP.attlocal.net>
2024-03-05 02:05:44 -0500

    Updated the server.js and the index.js thats in src folder. Change the App.js into App.jsx and update the file. The  App.test.js, logo.svg, reportWebVitals.js, and the setupTest.js files were deleted. Uninstalled web-vitals.

---
+++ config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI)
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    console.log(`Connectd to${db.name} at ${db.host}`)
+})
---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+// require('./config/database')
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+
+const app = express()
+
+
+const PORT = process.env.PORT || 3001
+
+app.use(logger('dev'))
+app.use(express.json())
+
+
+
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
+app.use(express.static(path.join(__dirname, 'build')))
+// app.use((req, res, next) => {
+//     res.locals.date = {}
+//     next()   
+// })
+// app.use('/api/items', require('./routes/api/items'))
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+
+app.listen(PORT, () => {
+    console.log(`I'm am listening on ${PORT}. We In the House`)
+})
